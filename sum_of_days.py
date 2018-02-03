2d5b3eb0d4baf423a3cd8917ce90a39e010bda05
zaiyangliu <lzyxn@126.com>
2018-02-03 21:09:45 +0800

    Create sum_of_days.py
    题目内容：
    根据下列信息计算在1901年1月1日至2000年12月31日间共有多少个星期天落在每月的第一天上？
    
    a)  1900.1.1是星期一
    b)  1月，3月，5月，7月，8月，10月和12月是31天
    c)  4月，6月，9月和11月是30天
    d)  2月是28天，在闰年是29天
    e)  公元年数能被4整除且又不能被100整除是闰年
    f)  能直接被400整除也是闰年

---
+++ sum_of_days.py
@@ -0,0 +1,36 @@
+#using python2.7
+day_Sum = 1  
+count = 0  
+for year in range(1900,2001):  
+    for month in range(1,13):  
+        if month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:  
+            day = 31  
+        elif month == 4 or month == 6 or month == 9 or month == 11:  
+            day = 30  
+        elif month == 2:  
+            if(year % 400 == 0)or(year % 4 == 0 and year % 100 == 0):  
+                day = 29  
+            else:  
+                day = 28  
+        day_Sum + = day  
+        if day_Sum % 7 == 0 and year > 1900:  
+            count + = 1  
+print count  
+#using python3.6
+day_Sum = 1  
+count = 0  
+for year in range(1900,2001):  
+    for month in range(1,13):  
+        if month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:  
+            day = 31  
+        elif month == 4 or month == 6 or month == 9 or month == 11:  
+            day=30  
+        elif month == 2:  
+            if(year%400 == 0)or(year%4 == 0 and year%100 == 0):  
+                day = 29  
+            else:  
+                day = 28  
+        day_Sum += day  
+        if day_Sum % 7 == 0 and year > 1900:
+            count += 1 
+print (count)  
